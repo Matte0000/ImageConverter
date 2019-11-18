1c42fb497bb8aa734a6b5179f690dee1d39676ce
Matteo Pagani <pagani220@gmail.com>
2019-11-18 14:44:25 +0100

    new features added

--- ImageConverter.py
+++ ImageConverter.py
@@ -3,7 +3,9 @@ import os
 from PIL import Image
 
 old_folder = sys.argv[1]
-new_folder = sys.argv[2]
+old_format = sys.argv[2]
+new_folder = sys.argv[3]
+new_format = sys.argv[4]
 
 if not os.path.exists(new_folder):
 	try:
@@ -16,8 +18,14 @@ if not os.path.exists(new_folder):
 for old_img in [name for name in os.listdir(old_folder) if os.path.isfile(os.path.join(old_folder, name))]:
 	if not os.path.exists(f'./{new_folder}/{old_img}'):
 		try:
-			img = Image.open(f'./{old_folder}/{old_img}')
-			img.save(f"./{new_folder}/{old_img.replace('.jpg', '')}.png")
-		except:
+			if old_img.split('.')[1] == old_format:
+				img = Image.open(f'./{old_folder}/{old_img}')
+				img.save(f"./{new_folder}/{old_img.replace(f'.{old_format}', '')}.{new_format}")
+			else:
+				os.rmdir(new_folder)
+				raise SyntaxError('old format is wrong')
+		except ValueError:
+			os.rmdir(new_folder)
 			print('Error to convert images')
+
 
